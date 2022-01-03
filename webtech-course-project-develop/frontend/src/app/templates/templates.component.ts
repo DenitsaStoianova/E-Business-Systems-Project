f24ec56c36be9aefa1b835f13a7480fd25511093
DenitsaStoianova <denitsa.stoianova@abv.bg>
2022-01-03 23:51:51 +0200

    Add templates base version

--- webtech-course-project-develop/frontend/src/app/templates/templates.component.ts
+++ webtech-course-project-develop/frontend/src/app/templates/templates.component.ts
@@ -8,21 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class TemplatesComponent implements OnInit {
   public sampleData = [{
     name: 'Template1',
-    imgUrl: '',
+    imgUrl: '90-day plan.png',
     description: '',
     price: 99.00,
     status: ''
   },
     {
       name: 'Template2',
-      imgUrl: '',
+      imgUrl: '90-day plan.png',
       description: '',
       price: 299.00,
       status: ''
     },
     {
       name: 'Template3',
-      imgUrl: '',
+      imgUrl: '90-day plan.png',
       description: '',
       price: 499.00,
       status: ''
@@ -36,5 +36,9 @@ export class TemplatesComponent implements OnInit {
     // eslint-disable-next-line no-param-reassign
     data.status = 'IN CART'; // appending a new key status
     //  this.sharedSerivce.setItemData(data);
+  }
+
+  openDoc(pdfUrl: string, startPage: number) {
+    window.open(pdfUrl + '#page=' + startPage, '_blank', '', true);
   }
 }
