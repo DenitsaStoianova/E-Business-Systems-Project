e4a3906a5d2164a277cffadc5c7e00b47e6bbcc1
kristinag9 <krisitsekova9@gmail.com>
2022-01-03 21:21:12 +0200

    Added make order modal

--- webtech-course-project-develop/frontend/src/app/continue-order-modal/continue-order-modal.component.ts
+++ webtech-course-project-develop/frontend/src/app/continue-order-modal/continue-order-modal.component.ts
@@ -1,25 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Route } from '../route.enum';
-import { User } from './user.interface';
-
+import Swal from 'sweetalert2';
 @Component({
     selector: 'continue-order-modal',
     templateUrl: './continue-order-modal.component.html',
     styleUrls: ['./continue-order-modal.component.css']
 })
 export class ContinueOrderModalComponent implements OnInit {
-    searchText = '';
-
     step: number = 1;
 
-    chatroomName: string = '';
+    userName: string = '';
 
-    chatroomDescription: string = '';
+    bankName: string = '';
 
-    users: User[] = [];
+    cardNumber: string = '';
 
-    members: User[] = [];
+    expiryDate: string = '';
 
     constructor(private readonly router: Router) {
     }
@@ -36,19 +33,8 @@ export class ContinueOrderModalComponent implements OnInit {
         this.step--;
     }
 
-    onSelectUser(user: User): void {
-        this.members = [...this.members, user];
-    }
-
-    onDeleteMember(member: User): void {
-        this.members.forEach((user: User, index: number) => {
-            if (user.email === member.email) {
-                this.members.splice(index, 1);
-            }
-        });
-    }
-
     onCreateChatroom(): void {
-        this.router.navigate([Route.Chatroom]);
+        Swal.fire('Successfully confirmed order!');
+        this.router.navigate([Route.Home]);
     }
 }
