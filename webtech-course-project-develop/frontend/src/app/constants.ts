bdf5b95f446280bf621d3ac9e9368c0a8a4af75e
DenitsaStoianova <denitsa.stoianova@abv.bg>
2022-01-29 20:10:30 +0200

    Add workspace buying

--- webtech-course-project-develop/frontend/src/app/constants.ts
+++ webtech-course-project-develop/frontend/src/app/constants.ts
@@ -2,7 +2,6 @@ export const NO_USER_LOGGED_IN_MESSAGE: string = 'You are currently not logged i
 export const TOKEN_LOCAL_STORAGE_KEY: string = 'token';
 export const BOUGHT_WORKSPACE_NAME: string = 'bought_workspace_name';
 export const CATEGORY_NAME: string = 'category_name';
-export const ITEM_INDICATOR: string = 'item_indicator';
 export const EXPIRES_AT_LOCAL_STORAGE_KEY: string = 'expires_at';
 export const USER_NAME_LOCAL_STORAGE_KEY: string = 'user_name';
 export const USER_WORKSPACE_NAME_BUY_TEMPLATE: string = 'no_workspace_name'
