354b22c482928f7c632c9de05e23fedf62ec4a2b
DenitsaStoianova <denitsa.stoianova@abv.bg>
2022-01-30 01:13:20 +0200

    Add workspace buying

--- webtech-course-project-develop/backend/src/models/boughtWorkspacesSchema.js
+++ webtech-course-project-develop/backend/src/models/boughtWorkspacesSchema.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 const boughtWorkspacesSchema = new mongoose.Schema(
     {
       type: String,
+      ownerName: String,
       department: String,
       maxPeople: Number,
       usersEmails: Array,
       templates: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'templates'
+            ref: 'Templates'
         }
     ]
     }
--- webtech-course-project-develop/frontend/src/app/profile-info/bought-workspace.interface.ts
+++ webtech-course-project-develop/frontend/src/app/profile-info/bought-workspace.interface.ts
@@ -2,6 +2,7 @@ import { Template } from "src/interfaces/template.interface";
 
 export interface BoughtWorkspace {
     type: string,
+    ownerName: string,
     department: string,
     maxPeople: number,
     usersEmails: Array<string>,
