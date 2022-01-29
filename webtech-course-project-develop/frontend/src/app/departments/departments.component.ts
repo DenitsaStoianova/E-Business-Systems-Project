65e706837b2fbf18bae4c77da0051027bc911ae2
SONYA STOYANOVA <sonya_stoyanova99@abv.bg>
2022-01-29 18:43:17 +0200

    Vizualize departments and categories

--- webtech-course-project-develop/frontend/src/app/departments/departments.component.ts
+++ webtech-course-project-develop/frontend/src/app/departments/departments.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import { Department } from './department.interface';
 import { Router } from '@angular/router';
 import { Route } from '../route.enum';
 import {CATEGORY_NAME} from "../constants";
@@ -9,8 +12,8 @@ import {CATEGORY_NAME} from "../constants";
   templateUrl: './departments.component.html',
   styleUrls: ['./departments.component.css']
 })
-export class DepartmentsComponent {
-  public sampleData = [{
+export class DepartmentsComponent implements OnInit{
+  /*public sampleData = [{
     name: 'IT Department',
     imgUrl: 'it_dept.png',
     firstCategory: 'Project management',
@@ -31,9 +34,24 @@ export class DepartmentsComponent {
       secondCategory: 'Researching',
       thirdCategory: 'Meetings'
     }];
+    */
 
- constructor(private readonly router: Router) {
+ departments: Array<Department> = [];
+
+ constructor(private readonly router: Router, private readonly httpClient: HttpClient) {
  }
+
+
+ ngOnInit() {
+  this.httpClient.get<Array<Department>>(environment.serveUrl + '/departments').subscribe(
+       (department: Array<Department>)=>{
+          for(let i = 0; i < department.length; ++i) {
+            console.log(department);
+            this.departments[i] = department[i]
+          }
+       }
+  );
+}
 
   navigateToTemplatesPage(category: string) {
    localStorage.setItem(CATEGORY_NAME, category);
