f381f67599642ce6f639b8336305487ec0abbc11
kristinag9 <krisitsekova9@gmail.com>
2022-01-30 02:46:16 +0200

    Backend refactoring

--- webtech-course-project-develop/backend/src/routes/departmentRouter.js
+++ webtech-course-project-develop/backend/src/routes/departmentRouter.js
@@ -4,6 +4,6 @@ const departmentController = require('../controllers/departmentController');
 const router = express.Router();
 
 router.get('/departments', departmentController.getDepartments);
-router.post('/departments', departmentController.createDepartment);
+router.post('/departments', departmentController.createDepartments);
 
 module.exports.departmentRouter = router;
